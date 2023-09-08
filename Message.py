1a9d6f335eff896dd673ff047209686a48e77ac6
Gabriel-Dee <gabriel.CSCU.2018@gmail.com>
2023-09-08 18:06:44 +0300

    changes

--- Message.py
+++ Message.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel
 
 # 2. Class which Message Details
 class Message(BaseModel):
-    message: string 
-    skewness: float 
-    curtosis: float
+    message: str
+    length: int
+    punct: int
